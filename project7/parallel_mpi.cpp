acb33e60d69a985cc72b29e9841a120ff539d67a
shanan2224466 <92194040+shanan2224466@users.noreply.github.com>
2024-07-08 15:23:28 +0800

    little update

--- project7/parallel_mpi.cpp
+++ project7/parallel_mpi.cpp
@@ -12,123 +12,123 @@ void mpi() {
 
     if (world_rank > 0) {
         while(complete_tasks < NUM_OF_TASKS) {
-			for(int worker = (world_rank - 1) * (NUM_OF_WORKERS / world_size); worker < world_rank * (NUM_OF_WORKERS / world_size); ++worker) {
-				int receipt = last_select_task[worker];
-				int local_load = last_local_load[worker];
+            for(int worker = (world_rank - 1) * (NUM_OF_WORKERS / world_size); worker < world_rank * (NUM_OF_WORKERS / world_size); ++worker) {
+                int receipt = last_select_task[worker];
+                int local_load = last_local_load[worker];
 
-				if(print_detail)
-					printf("Worker %d select task %d last time and remain %d works\n", worker, receipt, local_load);
+                if(print_detail)
+                    printf("Worker %d select task %d last time and remain %d works\n", worker, receipt, local_load);
 
-				if(local_load > 0) {
-					local_load -= workers[worker].task_efficient[receipt];
+                if(local_load > 0) {
+                    local_load -= workers[worker].task_efficient[receipt];
 
-					if(print_detail)
-						printf("Worker %d can finish %d works in unit time, now remain %d works\n", worker, workers[worker].task_efficient[receipt], local_load);
+                    if(print_detail)
+                        printf("Worker %d can finish %d works in unit time, now remain %d works\n", worker, workers[worker].task_efficient[receipt], local_load);
 
-					if(local_load <= 0) {
-						if(print_detail)
-							printf("Worker %d finish his work\n", worker);
+                    if(local_load <= 0) {
+                        if(print_detail)
+                            printf("Worker %d finish his work\n", worker);
 
-						local_load = 0;
+                        local_load = 0;
 
-						// Case 2: finish unit load
-						int lockNum[3] = {2, receipt, -1};
-						MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						MPI_Recv(&tasks[receipt].complete, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+                        // Case 2: finish unit load
+                        int lockNum[3] = {2, receipt, -1};
+                        MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        MPI_Recv(&tasks[receipt].complete, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
 
-						// Case 3: update task info
-						lockNum[0] = 3;
-						int localBuffer[3];
-						MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						MPI_Recv(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-						tasks[receipt].complete = localBuffer[0];
-						tasks[receipt].total_load = localBuffer[1];
-						tasks[receipt].end = localBuffer[2];
+                        // Case 3: update task info
+                        lockNum[0] = 3;
+                        int localBuffer[3];
+                        MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        MPI_Recv(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+                        tasks[receipt].complete = localBuffer[0];
+                        tasks[receipt].total_load = localBuffer[1];
+                        tasks[receipt].end = localBuffer[2];
 
-						if((tasks[receipt].complete >= tasks[receipt].total_load) && (tasks[receipt].end == 0)) {
-							tasks[receipt].end = 1;
-							complete_tasks++;
+                        if((tasks[receipt].complete >= tasks[receipt].total_load) && (tasks[receipt].end == 0)) {
+                            tasks[receipt].end = 1;
+                            complete_tasks++;
 
-							localBuffer[0] = 1;
-							localBuffer[1] = tasks[receipt].end;
-							localBuffer[2] = complete_tasks;
-							MPI_Send(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                            localBuffer[0] = 1;
+                            localBuffer[1] = tasks[receipt].end;
+                            localBuffer[2] = complete_tasks;
+                            MPI_Send(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
 
-							if(print_detail)
-								printf("\nTask %d **complete** by worker %d\n\n", receipt, worker);
-						} else {
-							localBuffer[0] = 0;
-							MPI_Send(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						}
+                            if(print_detail)
+                                printf("\nTask %d **complete** by worker %d\n\n", receipt, worker);
+                        } else {
+                            localBuffer[0] = 0;
+                            MPI_Send(localBuffer, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        }
 
-						// Case 4: take the next unit load
-						lockNum[0] = 4;
-						MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						MPI_Recv(localBuffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-						tasks[receipt].left = localBuffer[0];
-						if(tasks[receipt].left > 0) {
-							tasks[receipt].left -= UNIT_LOAD;
+                        // Case 4: take the next unit load
+                        lockNum[0] = 4;
+                        MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        MPI_Recv(localBuffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+                        tasks[receipt].left = localBuffer[0];
+                        if(tasks[receipt].left > 0) {
+                            tasks[receipt].left -= UNIT_LOAD;
 
-							local_load = UNIT_LOAD;
+                            local_load = UNIT_LOAD;
 
-							localBuffer[0] = 1;
-							localBuffer[1] = tasks[receipt].left;
-							MPI_Send(localBuffer, 2, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                            localBuffer[0] = 1;
+                            localBuffer[1] = tasks[receipt].left;
+                            MPI_Send(localBuffer, 2, MPI_INT, 0, tag, MPI_COMM_WORLD);
 
-							if(print_detail)
-								printf("Worker %d receives %d works from task %d\n", worker, local_load, receipt);
-						} else {
-							localBuffer[0] = 0;
-							MPI_Send(localBuffer, 2, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						}
-					}
-				}
+                            if(print_detail)
+                                printf("Worker %d receives %d works from task %d\n", worker, local_load, receipt);
+                        } else {
+                            localBuffer[0] = 0;
+                            MPI_Send(localBuffer, 2, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        }
+                    }
+                }
 
-				if(local_load <= 0) {
-					for(int select_task_id = 0; select_task_id < NUM_OF_TASKS; ++select_task_id) {
-						int s = workers[worker].prefer_task[select_task_id];
+                if(local_load <= 0) {
+                    for(int select_task_id = 0; select_task_id < NUM_OF_TASKS; ++select_task_id) {
+                        int s = workers[worker].prefer_task[select_task_id];
 
-						if(print_detail)
-							printf("Worker %d want to choose tasks %d\n", worker, s);
+                        if(print_detail)
+                            printf("Worker %d want to choose tasks %d\n", worker, s);
 
-						// Case 5: worker changes task and take unit load
-						int lockNum[3] = {5, s, last_select_task[worker]};
-						int localBuffer[4];
-						MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						MPI_Recv(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-						tasks[s].left = localBuffer[0];
-						tasks[s].current_num = localBuffer[1];
-						tasks[last_select_task[worker]].current_num = localBuffer[2];
-						if(tasks[s].left > 0 && tasks[s].current_num < tasks[s].max) {
-							tasks[s].current_num++;
-							receipt = s;
-							tasks[receipt].left -= UNIT_LOAD;
-							local_load = UNIT_LOAD;
+                        // Case 5: worker changes task and take unit load
+                        int lockNum[3] = {5, s, last_select_task[worker]};
+                        int localBuffer[4];
+                        MPI_Send(lockNum, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        MPI_Recv(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+                        tasks[s].left = localBuffer[0];
+                        tasks[s].current_num = localBuffer[1];
+                        tasks[last_select_task[worker]].current_num = localBuffer[2];
+                        if(tasks[s].left > 0 && tasks[s].current_num < tasks[s].max) {
+                            tasks[s].current_num++;
+                            receipt = s;
+                            tasks[receipt].left -= UNIT_LOAD;
+                            local_load = UNIT_LOAD;
 
-							if(receipt != -1) {
-								tasks[last_select_task[worker]].current_num--;
-							}
+                            if(receipt != -1) {
+                                tasks[last_select_task[worker]].current_num--;
+                            }
 
-							localBuffer[0] = 1;
-							localBuffer[1] = tasks[s].current_num;
-							localBuffer[2] = tasks[receipt].left;
-							localBuffer[3] = tasks[last_select_task[worker]].current_num;
-							MPI_Send(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                            localBuffer[0] = 1;
+                            localBuffer[1] = tasks[s].current_num;
+                            localBuffer[2] = tasks[receipt].left;
+                            localBuffer[3] = tasks[last_select_task[worker]].current_num;
+                            MPI_Send(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD);
 
-							if(print_detail)
-								printf("Worker %d receives task %d sucessfully and receives %d works from it\n", worker, receipt, local_load);
+                            if(print_detail)
+                                printf("Worker %d receives task %d sucessfully and receives %d works from it\n", worker, receipt, local_load);
 
-							select_task_id = NUM_OF_TASKS;
-						} else {
-							localBuffer[0] = 0;
-							MPI_Send(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD);
-						}
-					}
-				}
+                            select_task_id = NUM_OF_TASKS;
+                        } else {
+                            localBuffer[0] = 0;
+                            MPI_Send(localBuffer, 4, MPI_INT, 0, tag, MPI_COMM_WORLD);
+                        }
+                    }
+                }
 
-				last_select_task[worker] = receipt;
-				last_local_load[worker] = local_load;
-			}
+                last_select_task[worker] = receipt;
+                last_local_load[worker] = local_load;
+            }
 
             int check[3] = {6, -1, -1};
             MPI_Send(check, 3, MPI_INT, 0, tag, MPI_COMM_WORLD);
@@ -150,12 +150,12 @@ void mpi() {
                     world_time += TIMEPIECE;
                     MPI_Bcast(&world_time, 1, MPI_INT, 0, MPI_COMM_WORLD);
                     break;
-				// The task has completed unit load
+                // The task has completed unit load
                 case 2:
                     tasks[lockNum[1]].complete += UNIT_LOAD;
                     MPI_Send(&tasks[lockNum[1]].complete, 1, MPI_INT, status.MPI_SOURCE, tag, MPI_COMM_WORLD);
                     break;
-				// Update task info
+                // Update task info
                 case 3:
                     Buffer[0] = tasks[lockNum[1]].complete;
                     Buffer[1] = tasks[lockNum[1]].total_load;
@@ -167,7 +167,7 @@ void mpi() {
                         complete_tasks = Buffer[2];
                     }
                     break;
-				// Take the next unit load
+                // Take the next unit load
                 case 4:
                     buffer[0] = tasks[lockNum[1]].left;
                     MPI_Send(buffer, 1, MPI_INT, status.MPI_SOURCE, tag, MPI_COMM_WORLD);
@@ -176,7 +176,7 @@ void mpi() {
                         tasks[lockNum[1]].left = buffer[1];
                     }
                     break;
-				// Change worker's task and take unit load
+                // Change worker's task and take unit load
                 case 5:
                     temp[0] = tasks[lockNum[1]].left;
                     temp[1] = tasks[lockNum[1]].current_num;
